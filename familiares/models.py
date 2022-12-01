34804d58e88f49046baa20c95812e686abc52014
AleEnet <alejandro.enet1991@gmail.com>
2022-12-01 19:49:02 -0300

    Modelo creado y migrado

--- familiares/models.py
+++ familiares/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Familiares (models.Model):
+    Nombre = models.CharField(max_length= 100)
+    apellido = models.CharField(max_length= 450)
+    Edad = models.IntegerField()
+    image = models.ImageField(upload_to="familiares/images")
+    
