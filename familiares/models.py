ce112bc292e261cae9601529cf27fc3b3a280eb2
AleEnet <alejandro.enet1991@gmail.com>
2022-12-01 20:11:36 -0300

    familiares creados por admin

--- familiares/models.py
+++ familiares/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Familiares (models.Model):
-    Nombre = models.CharField(max_length= 100)
-    apellido = models.CharField(max_length= 450)
+class Familiare (models.Model):
+    Familiar = models.CharField(max_length= 50) 
+    Nombre = models.CharField(max_length= 50)
+    apellido = models.CharField(max_length= 50)
     Edad = models.IntegerField()
     image = models.ImageField(upload_to="familiares/images")
     
