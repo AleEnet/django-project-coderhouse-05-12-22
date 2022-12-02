5e49f134df32c131f16ad9a882c4bef0c3ab3a8b
AleEnet <alejandro.enet1991@gmail.com>
2022-12-01 23:36:43 -0300

    version final

--- djangoentregable/urls.py
+++ djangoentregable/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from familiares.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",  home) 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- familiares/views.py
+++ familiares/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Familiare
 
+def home (request):
+    familiar = Familiare.objects.all()
+    return render(request,"home.html", {"familiar" : familiar})
 # Create your views here.
